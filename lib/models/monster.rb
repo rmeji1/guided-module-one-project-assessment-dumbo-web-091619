69971755214573287c259f69595e58312212d917
github username <github email address>
2019-10-03 10:14:36 -0400

    Fixed monster class

--- lib/models/monster.rb
+++ lib/models/monster.rb
@@ -2,9 +2,9 @@ class Monster < ActiveRecord::Base
     has_many :fights
 
     def self.get_monster_that_hasnt_fought(character)
-        Monster.find_by(name: "dragon")
-        # character.reload
-        # avail_monsters = self.all - character.monsters.to_a
-        # avail_monsters.sample
+        # Monster.find_by(name: "dragon")
+        character.reload
+        avail_monsters = self.all - character.monsters.to_a
+        avail_monsters.sample
     end
 end
